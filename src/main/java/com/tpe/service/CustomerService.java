f203e5d6c570875514025287a73b185211d7104c
Muhammed Musab Demir <mmusabdemirr@gmail.com>
2023-06-15 19:51:37 +0300

    update

---
+++ src/main/java/com/tpe/service/CustomerService.java
@@ -0,0 +1,24 @@
+package com.tpe.service;
+
+import com.tpe.domain.Customer;
+import com.tpe.exception.ConflictException;
+import com.tpe.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public void saveCustomer(Customer customer) {
+        boolean isExistCustomer = customerRepository.existsByEmail(customer.getEmail());
+
+        if (isExistCustomer) {
+            throw new ConflictException("Customer already exist by " + customer.getEmail());
+        }
+
+        customerRepository.save(customer);
+    }
+}
